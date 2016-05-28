4281acd5e6e5bc1bf69df001b6aaf118cf0463f9
Tobias Reich <tobias.reich.ich@gmail.com>
2016-05-28 14:17:59 +0200

    Uppercase jsdoc types

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const path = require('path')
 
 /*
  * Rename the extension of a file in a path.
- * @param {string} filePath - Path to a file.
- * @param {string} newExt - New extension for the file.
- * @returns {string} filePath - Path ending with the new extension.
+ * @param {String} filePath - Path to a file.
+ * @param {String} newExt - New extension for the file.
+ * @returns {String} filePath - Path ending with the new extension.
  */
 module.exports = function(filePath, newExt) {
 
